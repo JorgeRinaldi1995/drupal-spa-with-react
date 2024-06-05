191d9b6ab2f996e04870ecdc0f0139fe69f30728
JorgeRinaldi1995 <jorge.rinaldi.1995@gmail.com>
2024-06-05 13:47:15 -0300

    base structure to react span for drupal

---
+++ src/utils/fetch.js
@@ -0,0 +1,29 @@
+/**
+ * Helper function wraps a normal fetch call with a fetch request that first
+ * retrieves a CSRF token and then adds the token as an X-CSRF-Token header
+ * to the options for desired fetch call.
+ *
+ * @param {string} csrfUrl
+ *   URL where we can retrieve a CSRF token for the current user.
+ * @param {string} fetchUrl
+ *   URL to fetch with X-CSRF-Token header included.
+ * @param {object} fetchOptions
+ *   Options to pass to fetch for the call to fetchUrl.
+ */
+export const fetchWithCSRFToken = (csrfUrl, fetchUrl, fetchOptions) => {
+  var basePath = drupalSettings.path.baseUrl;
+  var origin = window.location.origin;
+  var fullUrl = origin + basePath;
+
+  if (!fetchOptions.headers.get('X-CSRF-Token')) {
+    return fetch(`${fullUrl}${csrfUrl}`)
+      .then(response => response.text())
+      .then((csrfToken) => {
+        fetchOptions.headers.append('X-CSRF-Token', csrfToken);
+        return fetch(`${fullUrl}${fetchUrl}`, fetchOptions);
+      });
+  }
+  else {
+    return fetch(`${fullUrl}${fetchUrl}`, fetchOptions);
+  }
+};
